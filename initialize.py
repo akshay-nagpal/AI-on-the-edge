0ffcd4e218b075518eb6ec03f4332b09ff10e796
akshay-nagpal <akshay.ngpl10@gmail.com>
2021-04-04 18:43:35 +0530

    intialize file added

---
+++ initialize.py
@@ -0,0 +1,38 @@
+import subprocess
+import sys
+import os
+import time
+import schedule
+os.system("pip3 install schedule")
+os.system("pip3 install apscheduler")
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BlockingScheduler
+print ("start")
+ip=sys.argv[1]
+# user=sys.argv[2]
+# os.system("sudo apt update")
+os.system("sudo apt install nfs-common -y")
+os.system("sudo mkdir -p /mnt/nfs_clientshare")
+os.system("sudo mount 192.168.43.132:/mnt/nfs_share  /mnt/nfs_clientshare")
+os.system("sudo echo '192.168.43.132:/nfsshare /mnt  nfs defaults 0 0' >>sudo /etc/fstab")
+#os.system("sudo apt install sysbench")
+os.system("sudo apt-get install lm-sensors ")
+os.system("sudo apt install openssh-server")
+# os.system("ssh-keygen")
+# os.system("sudo sensors-detect ")
+os.system("sudo service kmod start")
+def run():
+   os.system("python3 /./mnt/nfs_clientshare/SPE_final_project/load.py "+ip+"")
+scheduler = BlockingScheduler()
+scheduler2=BackgroundScheduler()
+scheduler2.add_job(run,'cron',second='*/5')
+# scheduler.add_job(run, 'cron', second='*/5')
+scheduler2.start()
+# scheduler.every(5).seconds.do(run)0
+# while(True):
+#     schedule.run_pending()
+print ("end")
+
+
+
+
